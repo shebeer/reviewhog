4591e761315f7ce6fdbcc9be49206092eb4bc6e7
Dinesh Wagh <dinesh.wagh@numerator.com>
2023-07-20 14:50:00 +0530

    data access checkin

---
+++ ReviewHogNet/ReviewHogAPI/Controllers/ReviewhogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReviewHogAPI.Controllers
+{
+    public class ReviewhogController : ControllerBase
+    {
+        
+    }
+}
