02da30d5edf003938da2d3943c458e12d690ded3
Dinesh Wagh <dinesh.wagh@numerator.com>
2023-07-21 12:03:31 +0530

    ReviewHogMain added to have db connectivity

---
+++ ReviewHogMain/SentimentAnalyzer/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ReviewHogMainDB();
+            var listProduct = db.ProductDetails.ToList();
+        }
+    }
+}
