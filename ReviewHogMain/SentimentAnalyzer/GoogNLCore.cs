e7c6f96148d5ce70b23a80f1c5d522b5f7110f7e
Dinesh Wagh <dinesh.wagh@numerator.com>
2023-07-21 12:08:30 +0530

    new changes

---
+++ ReviewHogMain/SentimentAnalyzer/GoogNLCore.cs
@@ -0,0 +1,42 @@
+﻿using Google.Api.Gax.Grpc;
+using Google.Cloud.Language.V1;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalyzer
+{
+    public static class GoogNLCore
+    {
+        public static SentimentResponseModel AnalyzeText(string text)
+        {
+            LanguageServiceSettings settings = new LanguageServiceSettings
+            {
+                //TODO: Need to enter a key to make the API calls
+                CallSettings = CallSettings.FromHeader("X-Goog-Api-Key", "AIzaSyAm7yi5ZyyZdt8_rBKNRd63VE9dP4Nvh6o")
+            };
+
+            LanguageServiceClient client = new LanguageServiceClientBuilder
+            {
+                ChannelCredentials = ChannelCredentials.SecureSsl,
+                Settings = settings
+            }.Build();
+
+            Document document = Document.FromPlainText(text);
+            AnalyzeSentimentResponse response = client.AnalyzeSentiment(document);
+            AnalyzeEntitySentimentRequest entityRequest = new AnalyzeEntitySentimentRequest()
+            {
+                Document = document
+            };
+            AnalyzeEntitySentimentResponse entityResponse = client.AnalyzeEntitySentiment(entityRequest);
+
+            SentimentResponseModel result = new SentimentResponseModel();
+            result.MapFromGoogleSentimentResponse(response, entityResponse);
+
+            return result;
+        }
+    }
+}
---
+++ ReviewHogMain/SentimentAnalyzer/SentimentResponseModel.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Language.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalyzer
+{
+    public class SentimentResponseModel
+    {
+        public class EntityModel
+        {
+            public string Name { get; set; }
+            public float SentimentScore { get; set; }
+        }
+
+        public float SentimentScore { get; }
+        public float SentimentMagnitude { get; }
+        public Sentiment OverallSentiment { get; }
+        public List<EntityModel> Entities { get; }
+
+        public enum Sentiment
+        {
+            Negative = -1,
+            Neutral = 0,
+            Positive = 1
+        }
+
+        public void MapFromGoogleSentimentResponse(AnalyzeSentimentResponse sentimentResponse, AnalyzeEntitySentimentResponse entityResponse)
+        {
+
+        }
+    }
+}
