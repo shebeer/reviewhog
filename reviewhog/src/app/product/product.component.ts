d9d1a046ccb021f5aeab1686b6124383f66ad585
Shebeer M a <shebeer.ma@goperceptive.com>
2023-07-21 18:17:26 +0530

    Update desgin

--- reviewhog/src/app/product/product.component.ts
+++ reviewhog/src/app/product/product.component.ts
@@ -1,6 +1,31 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 
+
+interface PlatformInfo {
+  link: string;
+  avgRating: number;
+  totalRating: number;
+}
+
+interface Product {
+  title: string;
+  avgRating: number;
+  ratingScore: number;
+  sentiment: string;
+  description: string;
+  image: string;
+  id: string;
+  platforms: {
+    amazon: PlatformInfo;
+    walmart: PlatformInfo;
+    target: PlatformInfo;
+    samsclub: PlatformInfo;
+  };
+  positiveKeywords: string[];
+  negativeKeywords: string[];
+}
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -8,7 +33,39 @@ import { AppComponent } from '../app.component';
 })
 export class ProductComponent {
   constructor(private appComponent: AppComponent) {}
-  product: any = {}
+  product: Product = {
+    title: "",
+    avgRating: 0,
+    ratingScore: 0,
+    sentiment: "",
+    description: "",
+    image: "",
+    id: "",
+    platforms: {
+      amazon: {
+  link: "",
+  avgRating: 0,
+  totalRating: 0,
+},
+      walmart: {
+  link: "",
+  avgRating: 0,
+  totalRating: 0,
+},
+      target: {
+  link: "",
+  avgRating: 0,
+  totalRating: 0,
+},
+      samsclub: {
+  link: "",
+  avgRating: 0,
+  totalRating: 0,
+},
+    },
+    positiveKeywords: [],
+    negativeKeywords: [],
+  }
   ngOnInit(): void {
     this.product = this.appComponent.selectedItem
   }
