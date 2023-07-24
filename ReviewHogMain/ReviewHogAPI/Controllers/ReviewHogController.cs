26f649d0e14cf4c8a86b5c01a1bce9063ee32369
Dinesh Wagh <dinesh.wagh@numerator.com>
2023-07-24 17:31:31 +0530

    api changes

--- ReviewHogMain/ReviewHogAPI/Controllers/ReviewHogController.cs
+++ ReviewHogMain/ReviewHogAPI/Controllers/ReviewHogController.cs
@@ -1,13 +1,25 @@
 ﻿using DataModels;
+using LinqToDB;
+using ReviewHogAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace ReviewHogAPI.Controllers
 {
+
+    public enum SupermarketEnum : long
+    {
+        Amazon = 4,
+        Walmart = 3,
+        Target = 51,
+        Samsclub = 105,
+    }
+
     public class ReviewHogController : ApiController
     {
         ReviewHogMainDB _db;
@@ -28,6 +40,108 @@ namespace ReviewHogAPI.Controllers
         public List<ProductReview> GetProductReview(int productId)
         {
             return _db.ProductReviews.Where(x => x.ProductId == productId).ToList();
+        }
+
+        private Amazon GetAmazonRating(long product_id)
+        {
+            var result = _db.ProductDetails
+                .Where(x => x.Id == product_id && x.SupermarketId == (long)SupermarketEnum.Amazon)
+                .FirstOrDefault();
+
+            return new Amazon 
+            {
+                avgRating = Convert.ToDouble(result.StarRatingScore),
+                totalRating = Convert.ToInt32(result.TotalStarRatings),
+                link = "NA"
+            };
+        }
+
+        private Walmart GetWalmartRating(long product_id)
+        {
+            var result = _db.ProductDetails
+                .Where(x => x.Id == product_id && x.SupermarketId == (long)SupermarketEnum.Walmart)
+                .FirstOrDefault();
+
+            return new Walmart
+            {
+                avgRating = Convert.ToDouble(result.StarRatingScore),
+                totalRating = Convert.ToInt32(result.TotalStarRatings),
+                link = "NA"
+            };
+        }
+
+        private Target GetTargetRating(long product_id)
+        {
+            var result = _db.ProductDetails
+                .Where(x => x.Id == product_id && x.SupermarketId == (long)SupermarketEnum.Target)
+                .FirstOrDefault();
+
+            return new Target
+            {
+                avgRating = Convert.ToDouble(result.StarRatingScore),
+                totalRating = Convert.ToInt32(result.TotalStarRatings),
+                link = "NA"
+            };
+        }
+
+        private Samsclub GetSamsclubRating(long product_id)
+        {
+            var result = _db.ProductDetails
+                .Where(x => x.Id == product_id && x.SupermarketId == (long)SupermarketEnum.Samsclub)
+                .FirstOrDefault();
+
+            return new Samsclub
+            {
+                avgRating = Convert.ToDouble(result.StarRatingScore),
+                totalRating = Convert.ToInt32(result.TotalStarRatings),
+                link = "NA"
+            };
+        }
+
+        [HttpGet]
+        [Route("productdata")]
+        public List<ProductOverallDetails> GetOverallData()
+        {
+            List<ProductOverallDetails> prodList = new List<ProductOverallDetails>();
+
+            foreach (var supermarket in _db.Supermarkets.ToList())
+            {
+                Platforms platform = new Platforms();
+            }
+
+            foreach (var productDetail in _db.ProductDetails)
+            {
+                long prodId = productDetail.Id;
+                ProductOverallDetails pd = new ProductOverallDetails();
+
+                pd.id = productDetail.Upc;
+                pd.title = productDetail.Name;
+                pd.description = productDetail.BrandName;
+                pd.platforms = new Platforms
+                {
+                    amazon = GetAmazonRating(prodId),
+                    walmart = GetWalmartRating(prodId),
+                    target = GetTargetRating(prodId),
+                    samsclub = GetSamsclubRating(prodId)
+                };
+
+                var sentiments = _db.ProductSentiments.Where(x => x.ProductMappingId == prodId).FirstOrDefault();
+
+                pd.avgRating = Convert.ToDouble(sentiments.RatingScoreAvg);
+                pd.ratingScore = Convert.ToInt32(sentiments.SentimentScoreAvg);
+                pd.sentiment = sentiments.Sentiment;
+
+                var positiveEntity = _db.ProductEntities.Where(x => x.EntityAvgSentiment > 0 && x.ProductId == prodId)
+                                        .OrderByDescending(x => x.HitCount).Take(5).Select(x => x.EntityName);
+
+                var negativeEntity = _db.ProductEntities.Where(x => x.EntityAvgSentiment < 0 && x.ProductId == prodId)
+                                        .OrderByDescending(x => x.HitCount).Take(5).Select(x => x.EntityName);
+
+                pd.positiveKeywords = positiveEntity.ToList();
+                pd.negativeKeywords = negativeEntity.ToList();
+            }
+
+            return prodList;
         }
     }
 }
---
+++ ReviewHogMain/ReviewHogAPI/Models/ProductOverallDetails.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReviewHogAPI.Models
+{
+    public class Amazon
+    {
+        public string link { get; set; }
+        public double avgRating { get; set; }
+        public int totalRating { get; set; }
+    }
+
+    public class Platforms
+    {
+        public Amazon amazon { get; set; }
+        public Walmart walmart { get; set; }
+        public Target target { get; set; }
+        public Samsclub samsclub { get; set; }
+    }
+
+    public class ProductOverallDetails
+    {
+        public string title { get; set; }
+        public double avgRating { get; set; }
+        public int ratingScore { get; set; }
+        public string sentiment { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public string id { get; set; }
+        public Platforms platforms { get; set; }
+        public List<string> positiveKeywords { get; set; }
+        public List<string> negativeKeywords { get; set; }
+    }
+
+    public class Samsclub
+    {
+        public string link { get; set; }
+        public double avgRating { get; set; }
+        public int totalRating { get; set; }
+    }
+
+    public class Target
+    {
+        public string link { get; set; }
+        public double avgRating { get; set; }
+        public int totalRating { get; set; }
+    }
+
+    public class Walmart
+    {
+        public string link { get; set; }
+        public double avgRating { get; set; }
+        public int totalRating { get; set; }
+    }
+
+
+}
